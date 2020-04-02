016cf1d0aa325739d621496881a421799ecb70cf
布丁的老父亲 <ruiwei88888@163.com>
2020-04-02 19:41:54 +0800

    update

--- packages/new-learn-popular/index.js
+++ packages/new-learn-popular/index.js
@@ -1,8 +1,5 @@
 const { API } = require('new-learn-core')
 const axios = require('axios')
-
 const getPopularImg = () => axios.get(API)
 
 module.exports = getPopularImg
-
-getPopularImg().then(res => console.log(res.data))
